8cb4e56b7917d163125972def3d8cb7243ff55c8
Breno Pinheiro de Moura <143467828+BrenoIFCE@users.noreply.github.com>
2023-09-10 23:14:48 -0300

    Create Questão12.c

---
+++ Questão12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int numero;
+
+  for(numero = 1; numero < 21; numero++){
+
+    printf("%d\n", numero * 4); //tabuada do número 2, basta alterar o número 2 no printf para ter qualquer tabuada.
+  }
+}
